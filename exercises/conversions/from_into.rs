df658b9558925a0dc6ab9958254fd74a9bdc85d0
AuroBreeze <1732373074@aliyun.com>
2026-03-08 16:48:56 +0800

    update

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,19 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        let mut split = s.split(",");
+        if split.clone().count() != 2 {
+            return Person::default();
+        }
+        let name = split.next().unwrap_or("").to_string();
+        let age = split.next().unwrap_or("").parse::<usize>().unwrap_or(0);
+        if name == "" || age == 0 {
+            return Person::default();
+        }
+        Person { name, age }
     }
 }
 
